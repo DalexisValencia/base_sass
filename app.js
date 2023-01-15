d9cda5ce06cbcb929fe71ce20a21c1170dabe18d
DalexisValencia <d.alexis.valencia@gmail.com>
2023-01-14 21:22:50 -0500

    Añadiendo la configuración del paquete live-server

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 3000;
-const path = require('path');
+  var liveServer = require("live-server");
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
-})
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  var params = {
+      port: 8080, // Set the server port. Defaults to 8080.
+      //   host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
+      //   root: "/public", // Set root directory that's being served. Defaults to cwd.
+      //   open: false, // When false, it won't load your browser by default.
+      //   ignore: 'dist/scss/abstracts/_fonts.scss', // comma-separated string for paths to ignore
+      //   file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
+      wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
+      //   logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
+      // middleware: [function(req, res, next) { next(); }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
+  };
+  liveServer.start(params);
