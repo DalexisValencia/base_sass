1096e6ddf851f1367f928524beb88cbe4f92fd48
Duvan Valencia <d.alexis.valencia@gmail.com>
2023-11-04 13:56:03 -0500

    responsive video

--- dist/js/main.js
+++ dist/js/main.js
@@ -7,8 +7,14 @@
             $("body").toggleClass("no-scroll")
        });
 
+       //open / close modal para descargar juego
        $('button.close').on("click", function() {
         $(".modal").toggleClass("active");
+       });
+
+       $('.video-play').on("click", function() {
+        $("#trailer").trigger('play');
+        $('.video-overlay').addClass('hidden')
        });
     });
 })(jQuery);
