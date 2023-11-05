099b4520f64c2048b0faae7f13674ef4cf08af60
Duvan Valencia <d.alexis.valencia@gmail.com>
2023-11-04 20:50:18 -0500

    Fix modal order now

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,20 +1,26 @@
 (function ($) {
     $(document).ready(function () {
-       //Abrir menú
-       $('button.burger').on("click", function(){
+        //Abrir menú
+        $('button.burger').on("click", function () {
             $(this).toggleClass('open');
             $(".main-menu").toggleClass('active');
             $("body").toggleClass("no-scroll")
-       });
+        });
 
-       //open / close modal para descargar juego
-       $('button.close').on("click", function() {
-        $(".modal").toggleClass("active");
-       });
+        //open / close modal para descargar juego
+        $('button.close').on("click", function () {
+            $(".modal").toggleClass("active");
+        });
 
-       $('.video-play').on("click", function() {
-        $("#trailer").trigger('play');
-        $('.video-overlay').addClass('hidden')
-       });
+        $('.video-play').on("click", function () {
+            $("#trailer").trigger('play');
+            $('.video-overlay').addClass('hidden')
+        });
+
+        $('.order-now__btn').on("click", function () {
+            $(".modal").toggleClass("active");
+            $(window).scrollTop(0);
+            $("body").toggleClass("no-scroll");
+        });
     });
 })(jQuery);
