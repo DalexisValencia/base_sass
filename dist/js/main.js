aabe2acc5d5f3cb001ea1b2d69bfb66bc42423a5
Duvan Valencia <d.alexis.valencia@gmail.com>
2023-11-04 22:09:24 -0500

    add owlgalery to game page

--- dist/js/main.js
+++ dist/js/main.js
@@ -23,5 +23,46 @@
             $(window).scrollTop(0);
             $("body").toggleClass("no-scroll");
         });
+
+        function initCarouselOnGamePage() {
+            $owl = $('.game-page--gallery.owl-carousel');
+            console.log("estoy aqui!")
+            const carouselGameWrapper = $(".game-page--gallery");
+            if (carouselGameWrapper.length >= 1) {
+                $owl.trigger('destroy.owl.carousel');
+                setTimeout(() => {
+                    $owl.owlCarousel({
+                        loop: true,
+                        margin: 10,
+                        nav: true,
+                        responsive: {
+                            0: {
+                                items: 1
+                            },
+                            600: {
+                                items: 3
+                            },
+                            1000: {
+                                items: 5
+                            },
+                            1280: {
+                                items: 6
+                            },
+                            1700: {
+                                items: 7
+                            },
+                        }
+                    })
+                }, 1000);
+            }
+        }
+
+        initCarouselOnGamePage();
+
+        $(document).resize(function () {
+            // const currentPage = location.
+        });
+
+
     });
 })(jQuery);
